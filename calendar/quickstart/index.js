88f6c8b04600b84258e220c8c2a582df6baced72
Eric Koleda <ekoleda@google.com>
2018-05-10 10:26:09 -0400

    Fix Calendar quickstart

--- calendar/quickstart/index.js
+++ calendar/quickstart/index.js
@@ -18,7 +18,8 @@
 const fs = require('fs');
 const readline = require('readline');
 const {google} = require('googleapis');
-const OAuth2Client = google.auth.OAuth2;
+
+// If modifying these scopes, delete credentials.json.
 const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
 const TOKEN_PATH = 'credentials.json';
 
@@ -40,7 +41,8 @@ try {
 function authorize(credentials, callback) {
   const {client_secret, client_id, redirect_uris} = credentials.installed;
   let token = {};
-  const oAuth2Client = new OAuth2Client(client_id, client_secret, redirect_uris[0]);
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
 
   // Check if we have previously stored a token.
   try {
