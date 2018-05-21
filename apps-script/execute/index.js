898246c102d2d47d59f59cc6661c3153cc6dd0d6
Grant Timmerman <granttimmerman@gmail.com>
2018-05-21 14:53:06 -0700

    Add Apps Script Execution snippet

---
+++ apps-script/execute/index.js
@@ -0,0 +1,76 @@
+
+/**
+ * @license
+ * Copyright Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// [START apps_script_execute]
+/**
+ * Call an Apps Script function to list the folders in the user's root
+ * Drive folder.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function callAppsScript(auth) {
+  var scriptId = 'ENTER_YOUR_SCRIPT_ID_HERE';
+  var script = google.script('v1');
+
+  // Make the API request. The request object is included here as 'resource'.
+  script.scripts.run({
+    auth: auth,
+    resource: {
+      function: 'getFoldersUnderRoot'
+    },
+    scriptId: scriptId
+  }, function(err, resp) {
+    if (err) {
+      // The API encountered a problem before the script started executing.
+      console.log('The API returned an error: ' + err);
+      return;
+    }
+    if (resp.error) {
+      // The API executed, but the script returned an error.
+
+      // Extract the first (and only) set of error details. The values of this
+      // object are the script's 'errorMessage' and 'errorType', and an array
+      // of stack trace elements.
+      var error = resp.error.details[0];
+      console.log('Script error message: ' + error.errorMessage);
+      console.log('Script error stacktrace:');
+
+      if (error.scriptStackTraceElements) {
+        // There may not be a stacktrace if the script didn't start executing.
+        for (var i = 0; i < error.scriptStackTraceElements.length; i++) {
+          var trace = error.scriptStackTraceElements[i];
+          console.log('\t%s: %s', trace.function, trace.lineNumber);
+        }
+      }
+    } else {
+      // The structure of the result will depend upon what the Apps Script
+      // function returns. Here, the function returns an Apps Script Object
+      // with String keys and values, and so the result is treated as a
+      // Node.js object (folderSet).
+      var folderSet = resp.response.result;
+      if (Object.keys(folderSet).length == 0) {
+        console.log('No folders returned!');
+      } else {
+        console.log('Folders under your root folder:');
+        Object.keys(folderSet).forEach(function(id){
+          console.log('\t%s (%s)', folderSet[id], id);
+        });
+      }
+    }
+  });
+}
+// [END apps_script_execute]
