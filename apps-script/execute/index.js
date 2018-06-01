85f215e566bdaecb0c7d32986e99008ae48945d5
Grant Timmerman <timmerman+devrel@google.com>
2018-06-01 09:28:32 -0700

    Fix lint issues

--- apps-script/execute/index.js
+++ apps-script/execute/index.js
@@ -21,17 +21,17 @@
  *
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-function callAppsScript(auth) {
-  var scriptId = 'ENTER_YOUR_SCRIPT_ID_HERE';
-  var script = google.script('v1');
+function callAppsScript(auth) { // eslint-disable-line no-unused-vars
+  const scriptId = 'ENTER_YOUR_SCRIPT_ID_HERE';
+  const script = google.script('v1');
 
   // Make the API request. The request object is included here as 'resource'.
   script.scripts.run({
     auth: auth,
     resource: {
-      function: 'getFoldersUnderRoot'
+      function: 'getFoldersUnderRoot',
     },
-    scriptId: scriptId
+    scriptId: scriptId,
   }, function(err, resp) {
     if (err) {
       // The API encountered a problem before the script started executing.
@@ -44,14 +44,14 @@ function callAppsScript(auth) {
       // Extract the first (and only) set of error details. The values of this
       // object are the script's 'errorMessage' and 'errorType', and an array
       // of stack trace elements.
-      var error = resp.error.details[0];
+      const error = resp.error.details[0];
       console.log('Script error message: ' + error.errorMessage);
       console.log('Script error stacktrace:');
 
       if (error.scriptStackTraceElements) {
         // There may not be a stacktrace if the script didn't start executing.
-        for (var i = 0; i < error.scriptStackTraceElements.length; i++) {
-          var trace = error.scriptStackTraceElements[i];
+        for (let i = 0; i < error.scriptStackTraceElements.length; i++) {
+          const trace = error.scriptStackTraceElements[i];
           console.log('\t%s: %s', trace.function, trace.lineNumber);
         }
       }
@@ -60,12 +60,12 @@ function callAppsScript(auth) {
       // function returns. Here, the function returns an Apps Script Object
       // with String keys and values, and so the result is treated as a
       // Node.js object (folderSet).
-      var folderSet = resp.response.result;
+      const folderSet = resp.response.result;
       if (Object.keys(folderSet).length == 0) {
         console.log('No folders returned!');
       } else {
         console.log('Folders under your root folder:');
-        Object.keys(folderSet).forEach(function(id){
+        Object.keys(folderSet).forEach(function(id) {
           console.log('\t%s (%s)', folderSet[id], id);
         });
       }
--- apps-script/snippets/README.md
+++
@@ -1,8 +0,0 @@
-# Google Sheets Node.js Snippets
-
-Node.js snippets from the [Google Apps Script API Guide](https://developers.google.com/apps-script/api/how-tos/execute).
-
-## API Request Example
-
-Follow the ["Executing a function"](https://developers.google.com/apps-script/api/how-tos/execute#api_request_example
-) example to learn how to make an Apps Script API execution request.
