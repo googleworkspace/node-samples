c0510ebc799853e1dce21f31db9c1004963573a5
Eric Koleda <ekoleda@google.com>
2018-11-08 10:04:40 -0500

    Clarify where token.json comes from.

--- drive/activity/index.js
+++ drive/activity/index.js
@@ -22,6 +22,9 @@ const {google} = require('googleapis');
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/activity',
                 'https://www.googleapis.com/auth/drive.metadata.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
 const TOKEN_PATH = 'token.json';
 
 // Load client secrets from a local file.
--- sheets/quickstart/index.js
+++ sheets/quickstart/index.js
@@ -21,6 +21,9 @@ const {google} = require('googleapis');
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
 const TOKEN_PATH = 'token.json';
 
 // Load client secrets from a local file.
