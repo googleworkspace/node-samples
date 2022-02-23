81837d17870e18b235694e2521b999ad8ed02ae5
Christian Schalk <cschalk@google.com>
2022-02-22 18:40:16 -0800

    Node-examples-update

---
+++ forms/snippets/get_all_responses.js
@@ -0,0 +1,45 @@
+// Copyright 2022 Google LLC
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+'use strict';
+
+const path = require('path');
+const google = require('@googleapis/forms');
+const {
+  authenticate
+} = require('@google-cloud/local-auth');
+
+const formID = '<YOUR_FORM_ID>';
+
+async function runSample(query) {
+  const auth = await authenticate({
+    keyfilePath: path.join(__dirname, 'credentials.json'),
+    scopes: 'https://www.googleapis.com/auth/forms.responses.readonly'
+  });
+
+  const forms = google.forms({
+    version: 'v1beta',
+    auth: auth
+  });
+
+  const res = await forms.forms.responses.list({
+    formId: formID
+  });
+
+  console.log(res.data);
+  return res.data;
+}
+
+if (module === require.main) {
+  runSample().catch(console.error);
+}
+module.exports = runSample;
