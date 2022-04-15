082415a43ed56aa46e895e31db3f4ec0b658beb3
soheilv <soheilv@google.com>
2022-04-15 21:43:02 +0000

    Updated slide snippets Added default authentication, async/await and try catch to: slides_copy_presentation, slides_create_image, slides_create_presentation, slides_create_slide, slides_create_textbox_with_text

---
+++ slides/snippets/slides_copy_presentation.js
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// [START slides_copy_presentation]
+/**
+ * Copys a Google Slide presentation.
+ * @param {string} presentationId The presentation to copy.
+ * @param {string} copyTitle The new title.
+ */
+async function copyPresentation(presentationId, copyTitle) {
+  const {GoogleAuth} = require('google-auth-library');
+  const {google} = require('googleapis');
+
+  const auth = new GoogleAuth(
+    {scopes: 'https://www.googleapis.com/auth/drive'});
+
+  const service = google.drive({version: 'v2', auth});
+  let request = {
+    name: copyTitle,
+  };
+
+  try {
+    const driveResponse = await service.files.copy({
+      fileId: presentationId,
+      resource: request,
+    });
+    let presentationCopyId = driveResponse.data.id;
+    console.log('Created copied presentation with ID: ' + presentationCopyId);
+  } catch (err) {
+    // TODO (developer) - handle exception
+    throw err;
+  }
+}
+// [END slides_copy_presentation]
+
+copyPresentation('12zc4QWOtsJZ0weX3zFHj5O_IVRhXQYqOXjbia4hoXw4', 'copyTest');
