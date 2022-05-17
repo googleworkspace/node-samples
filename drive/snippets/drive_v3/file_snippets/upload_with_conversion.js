599bf20ceca797f8f6d997fb622dae252ff00c71
Anurag Sharma <aanursharm@google.com>
2022-05-17 11:38:16 +0530

    Update upload_with_conversion.js

--- drive/snippets/drive_v3/file_snippets/upload_with_conversion.js
+++ drive/snippets/drive_v3/file_snippets/upload_with_conversion.js
@@ -21,11 +21,11 @@
  * */
 async function uploadWithConversion() {
   const fs = require('fs');
-  // Get credentials and build service
-  // TODO (developer) - Use appropriate auth mechanism for your app
   const {GoogleAuth} = require('google-auth-library');
   const {google} = require('googleapis');
-
+  
+  // Get credentials and build service
+  // TODO (developer) - Use appropriate auth mechanism for your app
   const auth = new GoogleAuth({scopes: 'https://www.googleapis.com/auth/drive'});
   const service = google.drive({version: 'v3', auth});
   const fileMetadata = {
