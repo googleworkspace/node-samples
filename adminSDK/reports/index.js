2b78547b951782e6172c7352b8b96135ba3da1cb
anuraggoogler <aanursharm@google.com>
2022-05-18 07:40:06 +0000

    Implemented default auth

--- adminSDK/reports/index.js
+++ adminSDK/reports/index.js
@@ -15,100 +15,24 @@
  * limitations under the License.
  */
 // [START admin_sdk_reports_quickstart]
-const fs = require('fs');
-const readline = require('readline');
-const {google} = require('googleapis');
-
-// If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/admin.reports.audit.readonly'];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-const TOKEN_PATH = 'token.json';
-
-// Load client secrets from a local file.
-fs.readFile('credentials.json', (err, content) => {
-  if (err) return console.error('Error loading client secret file', err);
-
-  // Authorize a client with the loaded credentials, then call the
-  // Reports API.
-  authorize(JSON.parse(content), listLoginEvents);
-});
-
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- *
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
-  const oauth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
-
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oauth2Client, callback);
-    oauth2Client.credentials = JSON.parse(token);
-    callback(oauth2Client);
-  });
-}
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- *
- * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback to call with the authorized
- *     client.
- */
-function getNewToken(oauth2Client, callback) {
-  const authUrl = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oauth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
-      oauth2Client.credentials = token;
-      storeToken(token);
-      callback(oauth2Client);
-    });
-  });
-}
-
-/**
- * Store token to disk be used in later program executions.
- *
- * @param {Object} token The token to store to disk.
- */
-function storeToken(token) {
-  fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-    if (err) return console.warn(`Token not stored to ${TOKEN_PATH}`, err);
-    console.log(`Token stored to ${TOKEN_PATH}`);
-  });
-}
-
 /**
  * Lists the last 10 login events for the domain.
- *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-function listLoginEvents(auth) {
+async function listLoginEvents() {
+  const {GoogleAuth} = require('google-auth-library');
+  const {google} = require('googleapis');
+
+  // Get credentials and build service
+  // TODO (developer) - Use appropriate auth mechanism for your app
+  const auth = new GoogleAuth({scopes:
+        'https://www.googleapis.com/auth/admin.reports.usage.readonly'});
   const service = google.admin({version: 'reports_v1', auth});
-  service.activities.list({
-    userKey: 'all',
-    applicationName: 'login',
-    maxResults: 10,
-  }, (err, res) => {
-    if (err) return console.error('The API returned an error:', err.message);
+  try {
+    const res = await service.activities.list({
+      userKey: 'all',
+      applicationName: 'login',
+      maxResults: 10,
+    });
 
     const activities = res.data.items;
     if (activities.length) {
@@ -119,11 +43,11 @@ function listLoginEvents(auth) {
     } else {
       console.log('No logins found.');
     }
-  });
+  } catch (err) {
+    // TODO(developer) - Handle error
+    throw err;
+  }
 }
 // [END admin_sdk_reports_quickstart]
 
-module.exports = {
-  SCOPES,
-  listLoginEvents,
-};
+listLoginEvents();
--- adminSDK/reseller/index.js
+++ adminSDK/reseller/index.js
@@ -15,99 +15,22 @@
  * limitations under the License.
  */
 // [START admin_sdk_reseller_quickstart]
-const fs = require('fs');
-const readline = require('readline');
-const {google} = require('googleapis');
-
-// If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/apps.order'];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-const TOKEN_PATH = 'token.json';
-
-// Load client secrets from a local file.
-fs.readFile('credentials.json', (err, content) => {
-  if (err) return console.error('Error loading client secret file', err);
-
-  // Authorize a client with the loaded credentials, then call the
-  // G Suite Reseller API.
-  authorize(JSON.parse(content), listSubscriptions);
-});
-
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- *
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
-  const oauth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
-
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oauth2Client, callback);
-    oauth2Client.credentials = JSON.parse(token);
-    callback(oauth2Client);
-  });
-}
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- *
- * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback to call with the authorized
- *     client.
- */
-function getNewToken(oauth2Client, callback) {
-  const authUrl = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oauth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
-      oauth2Client.credentials = token;
-      storeToken(token);
-      callback(oauth2Client);
-    });
-  });
-}
-
-/**
- * Store token to disk be used in later program executions.
- *
- * @param {Object} token The token to store to disk.
- */
-function storeToken(token) {
-  fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-    if (err) return console.warn(`Token not stored to ${TOKEN_PATH}`, err);
-    console.log(`Token stored to ${TOKEN_PATH}`);
-  });
-}
 
 /**
  * Lists the first 10 subscriptions you manage.
- *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-function listSubscriptions(auth) {
-  const service = google.reseller({version: 'v1', auth});
-  service.subscriptions.list({
-    maxResults: 10,
-  }, (err, res) => {
-    if (err) return console.error('The API returned an error:', err.message);
+async function listSubscriptions() {
+  const {GoogleAuth} = require('google-auth-library');
+  const {google} = require('googleapis');
 
+  // Get credentials and build service
+  // TODO (developer) - Use appropriate auth mechanism for your app
+  const auth = new GoogleAuth({scopes: 'https://www.googleapis.com/auth/apps.order'});
+  const service = google.reseller({version: 'v1', auth});
+  try {
+    const res = await service.subscriptions.list({
+      maxResults: 10,
+    });
     const subscriptions = res.data.subscriptions;
     if (subscriptions.length) {
       console.log('Subscriptions:');
@@ -117,11 +40,11 @@ function listSubscriptions(auth) {
     } else {
       console.log('No subscriptions found.');
     }
-  });
+  } catch (err) {
+    // TODO(developer) - Handle error
+    throw err;
+  }
 }
 // [END admin_sdk_reseller_quickstart]
 
-module.exports = {
-  SCOPES,
-  listSubscriptions,
-};
+listSubscriptions();
