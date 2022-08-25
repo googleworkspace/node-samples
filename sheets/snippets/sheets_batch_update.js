ab963ceb5cc7dd2fd2e9eb8020a867242eafa565
Steve Bazyl <sbazyl@google.com>
2022-08-25 10:53:29 -0600

    fix: Update scope string
    Fix #201

--- sheets/snippets/sheets_batch_update.js
+++ sheets/snippets/sheets_batch_update.js
@@ -28,7 +28,7 @@ async function batchUpdate(spreadsheetId, title, find, replacement) {
   const {google} = require('googleapis');
 
   const auth = new GoogleAuth(
-      {scopes: 'https://www.googleapis.com/auth/spreadsheet'});
+      {scopes: 'https://www.googleapis.com/auth/spreadsheets'});
 
   const service = google.sheets({version: 'v4', auth});
   const requests = [];
