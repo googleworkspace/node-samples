6731dbc6e6fd4d4e73ab17bad93d569a33eecbca
Alice <aliceykim0828@gmail.com>
2025-07-10 14:07:07 -0400

    feat: Add sample scripts for CustomEmoji APIs (#1050)
    * feat: update GAPIC ssamples to be featured in custom emoji guide
    
    * feat: update GAPIC ssamples to be featured in custom emoji guide
    
    * feat: update GAPIC ssamples to be featured in custom emoji guide
    
    * feat: update GAPIC ssamples to be featured in custom emoji guide
    
    * feat: update GAPIC ssamples to be featured in custom emoji guide
    
    * feat: update versions for dependencies
    
    - open
    - google-auth-library
    - @google-apps/chat
    - @grpc/grpc-js
    
    * Apply suggestions from code review
    
    Co-authored-by: Vinay Vyas <69166360+vinay-google@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Alice Kim <kalice@google.com>
    Co-authored-by: Vinay Vyas <69166360+vinay-google@users.noreply.github.com>

---
+++ chat/client-libraries/cloud/create-custom-emoji-user-cred.js
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// It may require modifications to work in your environment.
+
+// [START chat_create_custom_emoji_user_cred]
+
+import {createClientWithUserCredentials} from './authentication-utils.js';
+import fs from 'fs';
+
+const USER_AUTH_OAUTH_SCOPES = ['https://www.googleapis.com/auth/chat.customemojis'];
+
+// This sample shows how to create custom emoji with user credential
+async function main() {
+  // Create a client
+  const chatClient = await createClientWithUserCredentials(USER_AUTH_OAUTH_SCOPES);
+
+  // TODO(developer) Replace FILENAME here.
+  const filename = 'FILENAME'
+  // Read Custom emoji file content into base64 encoded string
+  const fileContent = fs.readFileSync(filename, {encoding: 'base64'})
+
+  // Initialize request argument(s)
+  const request = {
+    custom_emoji: {
+      // TODO(developer): Replace EMOJI_NAME here.
+      emoji_name: ":EMOJI_NAME:",
+      payload: {
+        file_content: fileContent,
+        filename: filename,
+      }
+    }
+  };
+
+  // Make the request
+  const response = await chatClient.createCustomEmoji(request);
+
+  // Handle the response
+  console.log(response);
+}
+
+main().catch(console.error);
+
+// [END chat_create_custom_emoji_user_cred]
